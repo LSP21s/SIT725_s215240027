21fe140de5b60e9f5f4fff9cecd96ecbc34923e8
navodayaperera <s215240027@deakin.edu.au>
2025-11-22 14:39:13 +1100

    Fixed nested Git repo and added 2.2P correctly

---
+++ 2.2P/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const PORT = 3001;
+
+app.use(express.json());
+app.use(express.static('public')); // This will interact with the index.html file
+
+app.get('/add', (req, res) => {
+  const num1 = parseFloat(req.query.num1);
+  const num2 = parseFloat(req.query.num2);
+
+  if (isNaN(num1) || isNaN(num2)) {
+    return res.send("Please enter valid numbers.");
+  }
+  res.send(`The sum of ${num1} and ${num2} is: ${num1 + num2}`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});

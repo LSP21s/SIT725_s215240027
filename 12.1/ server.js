69694026e1344e0d70c314b965472b7a0e6f2342
navodayaperera <s215240027@deakin.edu.au>
2025-12-10 16:48:58 +1100

    adding initial files for group project

---
+++ 12.1/ server.js
@@ -0,0 +1,7 @@
+var express = require("express")
+var app = express()
+var port = process.env.port || 3000;
+$ git clone <link to repo>
+app.listen(port,()=>{
+console.log("App listening to: "+port)
+})
